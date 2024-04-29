97ecb8d924eb7c2b37e148cd72f73e26861b3d6f
roberthws <120668775+roberthws@users.noreply.github.com>
2024-04-29 18:51:59 -0300

    feat: adicionando algoritimo imprimir-valores-ate-50

---
+++ algoritimos/imprimir-valores-ate-50.js
@@ -0,0 +1,10 @@
+// 1 - Obrigatoriamente crie um For Loop que rode e imprima o valor de 0 até 50;
+// 2 - O For Loop é um laço de repetição.
+// 3 - Ele deve ter um "contador".
+// 4 - A cada "iteração" ele deve incrementar o contador caso seja correta a condição.
+
+for (let index = 0; index <= 50; index++) {
+  if (index % 2 === 0) {
+    console.log(index);
+  }
+}
