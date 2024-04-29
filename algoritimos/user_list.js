1768f85c1024d2a2b9765be2cde2f5f3cd802953
roberthws <120668775+roberthws@users.noreply.github.com>
2024-04-29 19:36:32 -0300

    feat: adicionando arquivo user_list

---
+++ algoritimos/user_list.js
@@ -0,0 +1,16 @@
+// 1 - Necessito criar um lista de usuários com os nomes
+// Sofia
+// Lucas
+// Isabella
+// Mateus
+// Alice
+//
+// Essa lista deve ser armazenada em uma constante.
+// Quero que na tela seja impresso na saída os nomes, Sofia, Isabella e Alice.
+// Dica: Será necessário utilizar um recurso chamado ARRAY
+
+const Array = ["Alice", "Sofia", "Lucas", "Isabela", "Mateus"];
+
+for (let index = 0; index < Array.length; index++) {
+  console.log("Usuário:", Array[index]);
+}
